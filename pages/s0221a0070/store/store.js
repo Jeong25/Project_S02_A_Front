952fddd3c88c3c168863ba8de1967793e47a3e23
JeongYeongBin <dudqls739@naver.com>
2022-10-27 14:28:33 +0900

    1027_로그추가

--- pages/s0221a0070/store/store.js
+++ pages/s0221a0070/store/store.js
@@ -3,7 +3,8 @@ import { retrieveCost } from '../repository/repository'
 const retrieveCostReq = async (memberId, confirmFromVal, confirmToVal, eventCode) => {
     const res = await retrieveCost(memberId, confirmFromVal, confirmToVal, eventCode)
         .catch((e) => console.log(JSON.stringify(e, null, 4)));
-    console.log('CostList Store Log: '+JSON.stringify(res?.data, null, 4));
+    console.log('CostList Store Log1: '+memberId,confirmFromVal,confirmToVal,eventCode);
+    console.log('CostList Store Log2: '+JSON.stringify(res?.data, null, 4));
     return res;
 }
 
