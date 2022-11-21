941a7fd5d6e366aa25ed7720fb81cd7fca5960cf
JeongYeongBin <dudqls739@naver.com>
2022-11-21 11:46:37 +0900

    1121

--- pages/common/api/client.js
+++ pages/common/api/client.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// 개발계
 const client = axios.create({ baseURL: 'http://3.37.212.202:8080' })
+// 운영계
+// const client = axios.create({ baseURL: 'http://3.38.235.59:8080' })
 
 export default client
